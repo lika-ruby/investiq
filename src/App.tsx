bbe6127ed06025a382050f33560a51ae5363bef4
lika-ruby <mary.havrylova.2011@gmail.com>
2026-06-27 13:03:23 +0300

    makes all

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 import { useEffect, lazy, Suspense } from "react";
+
 import { initAuthListener } from "./redux/users/usersOperations";
+import { useSelector } from "react-redux";
+import { selectIsAuth, selectAuthLoading } from "./redux/users/usersSelectors";
 
 const RegisterPage = lazy(() => import("./pages/RegisterPage"));
 const LoginPage = lazy(() => import("./pages/LoginPage"));
@@ -10,25 +13,31 @@ const CalculationsPage = lazy(() => import("./pages/CalculationsPage"));
 const AddPage = lazy(() => import("./pages/AddPage"));
 
 function App() {
+  const isAuth = useSelector(selectIsAuth);
+  const loading = useSelector(selectAuthLoading);
+
   useEffect(() => {
     initAuthListener();
   }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={isAuth ? <Layout /> : <Navigate to="/register" replace />}
+        >
           <Route index element={<HomePage />} />
-
           <Route path=":type" element={<HomePage />} />
-
           <Route path=":type/add" element={<AddPage />} />
-
           <Route path="calculations" element={<CalculationsPage />} />
           <Route path="calculations/:type" element={<CalculationsPage />} />
-
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="login" element={<LoginPage />} />
         </Route>
       </Routes>
     </Suspense>
